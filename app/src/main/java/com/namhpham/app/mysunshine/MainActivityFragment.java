e5b1f75c7fc37196d5fb30e3eef04851f5a3f1cb
Nam Pham <nampham@amazon.com>
2015-07-01 10:30:07 -0700

    Initial Commit.

---
+++ app/src/main/java/com/namhpham/app/mysunshine/MainActivityFragment.java
@@ -0,0 +1,48 @@
+package com.namhpham.app.mysunshine;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray =  new String[] {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Rainy - 80/50",
+                "Wednesday - Thunderstorm - 100/80",
+                "Thursday - Cloudy - 100/80",
+                "Friday - Foggy - 60/30",
+                "Saturday - Rainy - 100/50",
+                "Sunday - Sunny - 60/50"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                forecastArray);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(adapter);
+        return rootView;
+    }
+}
