7d929f75f314e981b8039b0c7d4157890adbb8f5
namhpham <namphamhoai@gmail.com>
2015-07-01 23:58:06 -0700

    2nd commit with AsyncTask code.

--- app/src/main/java/com/namhpham/app/mysunshine/MainActivity.java
+++ app/src/main/java/com/namhpham/app/mysunshine/MainActivity.java
@@ -1,7 +1,7 @@
 package com.namhpham.app.mysunshine;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -36,4 +36,7 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
